d8e4d1e64155d9b4bf933f181d3b70619a3c395f
carapidev <support@carapi.app>
2023-11-02 18:11:36 -0400

    get githubactions going

--- src/AuthDto.php
+++ src/AuthDto.php
@@ -3,24 +3,46 @@ declare(strict_types=1);
 
 namespace CarApiSdk;
 
+/**
+ * AuthDto
+ *
+ * @SuppressWarnings("unused")
+ */
 class AuthDto implements \JsonSerializable
 {
     private string $token;
     private string $secret;
 
+    /**
+     * Constructor
+     *
+     * @param string $token  Your token
+     * @param string $secret Your secret
+     */
     public function __construct(string $token, string $secret)
     {
         $this->token = $token;
         $this->secret = $secret;
     }
 
+    /**
+     * Builds an instance of this from the config instance.
+     *
+     * @param CarApiConfig $config The config
+     *
+     * @return self
+     */
     public static function build(CarApiConfig $config): self
     {
         return new self($config->token, $config->secret);
     }
 
     /**
+     * Serializes the instance into JSON.
+     *
+     * @see        \JsonSerializable
      * @inheritdoc
+     * @return     array
      */
     public function jsonSerialize(): array
     {
--- src/JsonSearch.php
+++ src/JsonSearch.php
@@ -3,10 +3,22 @@ declare(strict_types=1);
 
 namespace CarApiSdk;
 
+/**
+ * Holds JsonSearchItem instances
+ *
+ * @see JsonSearchItem
+ */
 class JsonSearch implements \JsonSerializable
 {
     private array $items = [];
 
+    /**
+     * Add a JSON search parameter
+     *
+     * @param JsonSearchItem $item An instance of JsonSearchItem
+     * 
+     * @return $this
+     */
     public function addItem(JsonSearchItem $item): self
     {
         $this->items[] = $item;
@@ -14,6 +26,13 @@ class JsonSearch implements \JsonSerializable
         return $this;
     }
 
+    /**
+     * Serializes the instance into JSON.
+     *
+     * @see        \JsonSerializable
+     * @inheritdoc
+     * @return     array
+     */
     public function jsonSerialize(): array
     {
         $return = [];
