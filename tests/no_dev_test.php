b6ca46d1ecdf66b963c6b58596f527813e73236d
carapidev <support@carapi.app>
2024-04-12 04:17:02 -0400

    debug pipeline

--- tests/no_dev_test.php
+++ tests/no_dev_test.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use CarApiSdk\CarApi;
-var_dump(getenv());
-var_dump(getenv('CARPI_TOKEN'));
+
 $sdk = CarApi::build([
-    'token' => getenv('CARPI_TOKEN'),
-    'secret' => getenv('CARPI_SECRET'),
+    'token' => 'test',
+    'secret' => 'test',
 ]);
-$sdk->authenticate();
-print_r($sdk->years(['query' => ['make' => 'Tesla']]));
 
 println('Done!');
